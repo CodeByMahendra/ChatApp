dd9206c25ab6b0402aeab568fd1dd470a8445240
Mahendra Chandel <mahendrachandel040@gmail.com>
2025-02-16 14:04:58 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`http://localhost:3005/api/v1/user/login`, user, {
+      const res = await axios.post(`https://chatapp-backend-ny29.onrender.com/api/v1/user/login`, user, {
         headers: {
           'Content-Type': 'application/json'
         },
