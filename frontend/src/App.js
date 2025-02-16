3de178c739c8e3101d38894cc85fdcb55d8f1270
Mahendra Chandel <mahendrachandel040@gmail.com>
2025-02-16 22:20:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
     let socketio;
 
     if (authUser) {
-      socketio = io(`${process.env.REACT_APP_BACKEND_URL}`, {
+      socketio = io(url, {
         query: {
           userId: authUser._id
         }
